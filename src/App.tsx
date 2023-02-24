34f4361d3f21ec31c885ef6dc5cfdf942990bc4b
Maksym Voloshyn <maxvol200727@gmail.com>
2023-02-24 18:53:54 +0200

    Add My profile

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,27 @@ import { Modal } from "./components/Modal";
 function App() {
 
   const [modal,setModal]=useState(false)
+  const [me,setMe]=useState(false)
   const [login,setLogin]=useState(false)
   const [email,setEmail]=useState("")
   const [error,setError]=useState(false)
-
+  const [memail, setMemail]=useState("")
+  const [name, setname]=useState("")
   const [password,setPassword]=useState("")  
   useEffect(()=>{
   if (localStorage.getItem("Token")) {
     setLogin(false)
+    axios.get("http://localhost:777/me",{
+      headers:{
+        authorization:localStorage.getItem("Token")
+      }
+    })
+    .then((res)=>{
+     setMemail(res.data.email)
+     setname(res.data.fullname)
+    })
   }  
-  })
-
+  },[])
   const changeEmail= (event: React.ChangeEvent<HTMLInputElement>)=>{
     setEmail(event.target.value)
     setError(false)
@@ -48,9 +58,16 @@ function App() {
     <input type="submit" value="Login" className="bg-[#5aff3199] py-3" onClick={()=>{Login()}}/>
   </form>
 </Modal>}
+{me&&<Modal title="Me">
+  <div className="px-10">
+  <div className="text-xl text-black">Your email: {memail}</div>
+  <div className="text-xl text-black">Your fullname: {name}</div>
+
+  </div>
+</Modal>}
 <div className="">
 <header className='text-[96px] ml-9 flex flex-row justify-around'>TODO 
-{login&&<button className='text-[50px] bg-black ml-9 rounded px-5 py-3' onClick={()=>setModal(true)}>SignUp</button>}
+{login?(<button className='text-[50px] bg-black ml-9 rounded px-5 py-3' onClick={()=>setModal(true)}>SignUp</button>):(<button className='text-[50px] bg-black ml-9 rounded px-5 py-3' onClick={()=>setMe(true)}>Me</button>)}
 </header>
 
 </div>
